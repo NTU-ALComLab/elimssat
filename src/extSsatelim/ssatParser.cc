d2cfb509d30e29fdace8ce318708ce5f0075ffd9
haorenW1025 <whz861025@gmail.com>
2022-03-18 22:44:51 +0800

    format ssatParser

--- src/extSsatelim/ssatParser.cc
+++ src/extSsatelim/ssatParser.cc
@@ -4,7 +4,7 @@
   PackageName []
   Synopsis    []
   Author      [Kuan-Hua Tu]
-  
+
   Affiliation []
   Date        [Sun Jun 23 02:39:25 CST 2019]
 ***********************************************************************/
@@ -13,13 +13,14 @@
 ///                          INCLUDES                                ///
 ////////////////////////////////////////////////////////////////////////
 
-#include "ssatelim.h"
 #include "extUtil/util.h"
-#include <zlib.h>
-#include <iostream>
+#include "formula.hpp"
+#include "ssatelim.h"
 #include <algorithm>
 #include <fstream>
+#include <iostream>
 #include <string>
+#include <zlib.h>
 // #include <math.h>
 #include <math.h>
 using namespace std;
@@ -31,6 +32,7 @@ ABC_NAMESPACE_IMPL_START
 ////////////////////////////////////////////////////////////////////////
 
 static const int DEBUG_PRINT = 0;
+static hqspre::Formula formula;
 
 ////////////////////////////////////////////////////////////////////////
 ///                     FUNCTION DEFINITIONS                         ///
@@ -41,10 +43,9 @@ static int PREFIX_NUM = 0;
 static int EXIST_NUM = 0;
 static int RANDOM_NUM = 0;
 
-static void error(const char* const msg)
-{
-    std::cerr << msg << "\n";
-    exit(255);
+static void error(const char *const msg) {
+  std::cerr << msg << "\n";
+  exit(255);
 }
 
 int ssat_addScope(ssat_solver *s, int *begin, int *end, QuantifierType type) {
@@ -96,245 +97,219 @@ int ssat_addclause(ssat_solver *s, lit *begin, lit *end) {
   return 1;
 }
 
+void ssat_readHeader_cnf(ssat_solver *s, string &str_in) {
+  string str = str_in.substr(6); // for the length of str "p cnf "
+  std::string::size_type sz = 0;
 
-void ssat_readHeader_cnf(ssat_solver * s, string& str_in)
-{
-    string str = str_in.substr(6); // for the length of str "p cnf "
-    std::string::size_type sz = 0;
-
-    long long nVar = stoll (str, &sz);
-    str = str.substr(sz);
-    if (DEBUG_PRINT) std::cout << " nVar: " << nVar << '\n';
-    ssat_solver_setnvars(s, nVar);
-    long long nClause = stoll (str, &sz);
-    if (DEBUG_PRINT) std::cout << " nClause: " << nClause << '\n';
+  long long nVar = stoll(str, &sz);
+  str = str.substr(sz);
+  if (DEBUG_PRINT)
+    std::cout << " nVar: " << nVar << '\n';
+  ssat_solver_setnvars(s, nVar);
+  long long nClause = stoll(str, &sz);
+  if (DEBUG_PRINT)
+    std::cout << " nClause: " << nClause << '\n';
 }
 
-void ssat_readHeader(ssat_solver * s, string& str)
-{
-    ssat_readHeader_cnf(s, str);
+void ssat_readHeader(ssat_solver *s, string &str) {
+  ssat_readHeader_cnf(s, str);
 }
 
-void ssat_readExistence(ssat_solver * s, string& str_in)
-{
-    Vec_Int_t * p = Vec_IntAlloc(0);
-    string str = str_in.substr(2); // for the length of str "e "
-    std::string::size_type sz = 0;
-    if (DEBUG_PRINT) cout << "e:" << endl;
+void ssat_readExistence(ssat_solver *s, string &str_in) {
+  Vec_Int_t *p = Vec_IntAlloc(0);
+  string str = str_in.substr(2); // for the length of str "e "
+  std::string::size_type sz = 0;
+  if (DEBUG_PRINT)
+    cout << "e:" << endl;
 
-    while (str.length())
-    {
-        long long var = stoll (str, &sz);
-        if (var == 0)
-        {
-            break;
-        }
-
-        if (DEBUG_PRINT) cout << " var:" << var << endl;
-        Vec_IntPush(p, var);
-        str = str.substr(sz + 1);
-        while (str[0] == ' ')
-        {
-            str = str.substr(1);
-        }
+  while (str.length()) {
+    long long var = stoll(str, &sz);
+    if (var == 0) {
+      break;
     }
 
-    if (Vec_IntSize(p) > 0)
-    {
-        ssat_addexistence(s, Vec_IntArray(p), Vec_IntLimit(p));
-    }
-    Vec_IntFree(p);
-}
-
-void ssat_readForall(ssat_solver * s, string& str_in)
-{
-    Vec_Int_t * p = Vec_IntAlloc(0);
-    string str = str_in.substr(2); // for the length of str "a "
-    std::string::size_type sz = 0;
-    if (DEBUG_PRINT) cout << "a:" << endl;
-
-    while (str.length())
-    {
-        long long var = stoll (str, &sz);
-        if (var == 0)
-        {
-            break;
-        }
-
-        if (DEBUG_PRINT) cout << " var:" << var << endl;
-        Vec_IntPush(p, var);
-        str = str.substr(sz + 1);
-        while (str[0] == ' ')
-        {
-            str = str.substr(1);
-        }
-    }
-
-    if (Vec_IntSize(p) > 0)
-    {
-        ssat_addforall(s, Vec_IntArray(p), Vec_IntLimit(p));
-    }
-    Vec_IntFree(p);
-}
-
-void ssat_readRandom(ssat_solver * s, string& str_in)
-{
-    Vec_Int_t * p = Vec_IntAlloc(0);
-    string str = str_in.substr(2); // for the length of str "r "
-    std::string::size_type sz = 0;
-    if (DEBUG_PRINT) cout << "r:" << endl;
-
-    double prob = stod (str, &sz);
-    if (DEBUG_PRINT) cout << " prob:" << prob << endl;
+    if (DEBUG_PRINT)
+      cout << " var:" << var << endl;
+    Vec_IntPush(p, var);
     str = str.substr(sz + 1);
-
-    while (str.length())
-    {
-        long long var = stoll (str, &sz);
-        if (var == 0)
-        {
-            break;
-        }
-
-        if (DEBUG_PRINT) cout << " var:" << var << endl;
-        Vec_IntPush(p, var);
-        str = str.substr(sz + 1);
-        while (str[0] == ' ')
-        {
-            str = str.substr(1);
-        }
+    while (str[0] == ' ') {
+      str = str.substr(1);
     }
+  }
 
-    if (Vec_IntSize(p) > 0)
-    {
-        ssat_addrandom(s, Vec_IntArray(p), Vec_IntLimit(p), prob);
-    }
-    Vec_IntFree(p);
+  if (Vec_IntSize(p) > 0) {
+    ssat_addexistence(s, Vec_IntArray(p), Vec_IntLimit(p));
+  }
+  Vec_IntFree(p);
 }
 
-void ssat_readClause(ssat_solver * s, string& str_in)
-{
-    Vec_Int_t * p = Vec_IntAlloc(0);
-    string str = str_in;
-    std::string::size_type sz = 0;
+void ssat_readForall(ssat_solver *s, string &str_in) {
+  Vec_Int_t *p = Vec_IntAlloc(0);
+  string str = str_in.substr(2); // for the length of str "a "
+  std::string::size_type sz = 0;
+  if (DEBUG_PRINT)
+    cout << "a:" << endl;
 
-    while (str.length())
-    {
-        long long lit = stoll (str, &sz);
-        if (lit == 0)
-        {
-            break;
-        }
-
-        if (DEBUG_PRINT) cout << "lit:" << lit << endl;
-        Vec_IntPush(p, toLitCond( abs(lit), signbit(lit) ));
-        str = str.substr(sz + 1);
-        while (str[0] == ' ')
-        {
-            str = str.substr(1);
-        }
+  while (str.length()) {
+    long long var = stoll(str, &sz);
+    if (var == 0) {
+      break;
     }
 
-    ssat_addclause(s, Vec_IntArray(p), Vec_IntLimit(p));
-    Vec_IntFree(p);
+    if (DEBUG_PRINT)
+      cout << " var:" << var << endl;
+    Vec_IntPush(p, var);
+    str = str.substr(sz + 1);
+    while (str[0] == ' ') {
+      str = str.substr(1);
+    }
+  }
+
+  if (Vec_IntSize(p) > 0) {
+    ssat_addforall(s, Vec_IntArray(p), Vec_IntLimit(p));
+  }
+  Vec_IntFree(p);
 }
 
-void ssat_lineHandle(ssat_solver * s, string& in_str)
-{
-    if (in_str.length() == 0 || in_str[0] == '\n' || in_str[0] == '\r')
-    {
-        return;
+void ssat_readRandom(ssat_solver *s, string &str_in) {
+  Vec_Int_t *p = Vec_IntAlloc(0);
+  string str = str_in.substr(2); // for the length of str "r "
+  std::string::size_type sz = 0;
+  if (DEBUG_PRINT)
+    cout << "r:" << endl;
+
+  double prob = stod(str, &sz);
+  if (DEBUG_PRINT)
+    cout << " prob:" << prob << endl;
+  str = str.substr(sz + 1);
+
+  while (str.length()) {
+    long long var = stoll(str, &sz);
+    if (var == 0) {
+      break;
     }
-    else if (in_str[0] == 'c')
-    {
-        // skip command line
-        return;
+
+    if (DEBUG_PRINT)
+      cout << " var:" << var << endl;
+    Vec_IntPush(p, var);
+    str = str.substr(sz + 1);
+    while (str[0] == ' ') {
+      str = str.substr(1);
     }
-    else if (in_str[0] == 'p')
-    {
-        ssat_readHeader(s, in_str);
-    }
-    else if (in_str[0] == 'e')
-    {
-        ssat_readExistence(s, in_str);
-    }
-    else if (in_str[0] == 'a')
-    {
-        ssat_readForall(s, in_str);
-    }
-    else if (in_str[0] == 'r')
-    {
-        ssat_readRandom(s, in_str);
-    }
-    else if (in_str[0] == '-' || isdigit(in_str[0]))
-    {
-        CLAUSE_NUM++;
-        ssat_readClause(s, in_str);
-    }
-    else
-    {
-        cout << "Error: Unexpected symbol" << endl;
-    }   
+  }
+
+  if (Vec_IntSize(p) > 0) {
+    ssat_addrandom(s, Vec_IntArray(p), Vec_IntLimit(p), prob);
+  }
+  Vec_IntFree(p);
 }
 
-void process(ssat_solver * s, gzFile in)
-{
-    char buf[BUFLEN];
-    char* offset = buf;
+void ssat_readClause(ssat_solver *s, string &str_in) {
+  Vec_Int_t *p = Vec_IntAlloc(0);
+  string str = str_in;
+  std::string::size_type sz = 0;
 
-    for (;;) {
-        int err, len = sizeof(buf)-(offset-buf);
-        if (len == 0) error("Buffer to small for input line lengths");
-
-        len = gzread(in, offset, len);
-
-        if (len == 0) break;    
-        if (len <  0) error(gzerror(in, &err));
-
-        char* cur = buf;
-        char* end = offset+len;
-
-        for (char* eol; (cur<end) && (eol = std::find(cur, end, '\n')) < end; cur = eol + 1)
-        {
-            string in_str = string(cur, eol);
-            ssat_lineHandle(s, in_str);
-        }
-
-        // any trailing data in [eol, end) now is a partial line
-        offset = std::copy(cur, end, buf);
+  while (str.length()) {
+    long long lit = stoll(str, &sz);
+    if (lit == 0) {
+      break;
     }
 
-    // BIG CATCH: don't forget about trailing data without eol :)
-    std::cout << std::string(buf, offset);
+    if (DEBUG_PRINT)
+      cout << "lit:" << lit << endl;
+    Vec_IntPush(p, toLitCond(abs(lit), signbit(lit)));
+    str = str.substr(sz + 1);
+    while (str[0] == ' ') {
+      str = str.substr(1);
+    }
+  }
 
-    if (gzclose(in) != Z_OK) error("failed gzclose");
+  ssat_addclause(s, Vec_IntArray(p), Vec_IntLimit(p));
+  Vec_IntFree(p);
 }
 
-void ssat_Parser(ssat_solver * s, char * filename)
-{
-    if (filename == NULL)
-    {
-        return;
-    }
-
-    process(s, gzopen(filename, "rb"));
-    int entry, index;
-    Vec_IntForEachEntry(s->pQuanType, entry, index) {
-      PREFIX_NUM++;
-      if (entry == Quantifier_Exist) {
-        EXIST_NUM += Vec_IntSize((Vec_Int_t*)(Vec_PtrEntry(s->pQuan, index)));
-      } else {
-        RANDOM_NUM += Vec_IntSize((Vec_Int_t*)(Vec_PtrEntry(s->pQuan, index)));
-      }
-    }
-    printf("\n");
-    printf("==== benchmark statistics ====\n");
-    printf("  > Number of Prefixes                = %d\n", PREFIX_NUM);
-    printf("  > Number of Existitential Variables = %d\n", EXIST_NUM);
-    printf("  > Number of Randomize Variables     = %d\n", RANDOM_NUM);
-    printf("  > Number of Clauses                 = %d\n", CLAUSE_NUM);
-
+void ssat_lineHandle(ssat_solver *s, string &in_str) {
+  if (in_str.length() == 0 || in_str[0] == '\n' || in_str[0] == '\r') {
     return;
+  } else if (in_str[0] == 'c') {
+    // skip command line
+    return;
+  } else if (in_str[0] == 'p') {
+    ssat_readHeader(s, in_str);
+  } else if (in_str[0] == 'e') {
+    ssat_readExistence(s, in_str);
+  } else if (in_str[0] == 'a') {
+    ssat_readForall(s, in_str);
+  } else if (in_str[0] == 'r') {
+    ssat_readRandom(s, in_str);
+  } else if (in_str[0] == '-' || isdigit(in_str[0])) {
+    CLAUSE_NUM++;
+    ssat_readClause(s, in_str);
+  } else {
+    cout << "Error: Unexpected symbol" << endl;
+  }
+}
+
+void process(ssat_solver *s, gzFile in) {
+
+  char buf[BUFLEN];
+  char *offset = buf;
+
+  for (;;) {
+    int err, len = sizeof(buf) - (offset - buf);
+    if (len == 0)
+      error("Buffer to small for input line lengths");
+
+    len = gzread(in, offset, len);
+
+    if (len == 0)
+      break;
+    if (len < 0)
+      error(gzerror(in, &err));
+
+    char *cur = buf;
+    char *end = offset + len;
+
+    for (char *eol; (cur < end) && (eol = std::find(cur, end, '\n')) < end;
+         cur = eol + 1) {
+      string in_str = string(cur, eol);
+      ssat_lineHandle(s, in_str);
+    }
+
+    // any trailing data in [eol, end) now is a partial line
+    offset = std::copy(cur, end, buf);
+  }
+
+  // BIG CATCH: don't forget about trailing data without eol :)
+  std::cout << std::string(buf, offset);
+
+  if (gzclose(in) != Z_OK)
+    error("failed gzclose");
+}
+
+void ssat_Parser(ssat_solver *s, char *filename) {
+  if (filename == NULL) {
+    return;
+  }
+
+  process(s, gzopen(filename, "rb"));
+  int entry, index;
+  Vec_IntForEachEntry(s->pQuanType, entry, index) {
+    PREFIX_NUM++;
+    if (entry == Quantifier_Exist) {
+      EXIST_NUM += Vec_IntSize((Vec_Int_t *)(Vec_PtrEntry(s->pQuan, index)));
+    } else {
+      RANDOM_NUM += Vec_IntSize((Vec_Int_t *)(Vec_PtrEntry(s->pQuan, index)));
+    }
+  }
+  printf("\n");
+  printf("==== benchmark statistics ====\n");
+  printf("  > Number of Prefixes                = %d\n", PREFIX_NUM);
+  printf("  > Number of Existitential Variables = %d\n", EXIST_NUM);
+  printf("  > Number of Randomize Variables     = %d\n", RANDOM_NUM);
+  printf("  > Number of Clauses                 = %d\n", CLAUSE_NUM);
+
+  return;
 }
 
 ABC_NAMESPACE_IMPL_END
